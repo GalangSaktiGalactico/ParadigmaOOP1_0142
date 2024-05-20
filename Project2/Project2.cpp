8cd1cbbb05862ea8f2845d31313f426f8b77fb79
Galang Sakti G <wadepakmen97@gmail.com>
2024-05-20 11:05:09 +0700

    public

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -7,3 +7,9 @@ private:
 	const int id;
 	string nama;
 	float nilai;
+
+public:
+
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama) { //member initialization list
+		//definisi
+	}
